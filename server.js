62c3115e302a2b48ef7a43758c900fa601d8e1e0
Matheus Henrique <matheus.hsilvaa18@gmail.com>
2025-09-15 12:59:29 -0300

    add missing newline for code readability

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 8080;
 app.use(helmet({
   contentSecurityPolicy: false // Permitir inline scripts para simplicidade
 }));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
